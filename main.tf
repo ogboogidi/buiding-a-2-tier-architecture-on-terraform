90b789e10fd0cd378c6114942a81ef55d1362888
ogboogidi <rsafo@empiredist.com>
2026-05-07 17:55:27 +0000

    fix s3 creation issues

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "terraform-github-actiontf"
+    bucket         = "terraform-github-action-workflow"
     key            = "terraform_tfstate"
     region         = "us-east-1"
     dynamodb_table = "tf.statelocking"
